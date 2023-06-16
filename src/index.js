e78f3a4741f6f57c8f7431638ea52e7cafb75425
Raquelalt <130921818+Raquelalt@users.noreply.github.com>
2023-06-16 19:42:15 +0200

    add js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function updateTime() {
+  let zurichElement = document.querySelector("#zurich");
+  let zurichDate = zurichElement.querySelector(".date");
+  let zurichTimeElement = zurichElement.querySelector(".time");
+  zurichDate.innerHTML = moment().format("MMMM Do YYYY");
+  let zurichTime = moment().tz("Europe/Zurich");
+  zurichTimeElement.innerHTML = zurichTime.format(
+    "h:mm:ss [<small>]A[</small>"
+  );
+  let lisbonElement = document.querySelector("#lisbon");
+  let lisbonDate = lisbonElement.querySelector(".date");
+  let lisbonTimeElement = lisbonElement.querySelector(".time");
+  lisbonDate.innerHTML = moment().format("MMMM Do YYYY");
+  let lisbonTime = moment().tz("Europe/Lisbon");
+  lisbonTimeElement.innerHTML = lisbonTime.format(
+    "h:mm:ss [<small>]A[</small>"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
